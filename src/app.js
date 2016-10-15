309ec9ee0ef73573112fba4dce508b865fb921e4
siriwanp <siriwan_lookpla@hotmail.com>
2016-10-15 12:04:10 +0700

    display dynamic list

--- src/app.js
+++ src/app.js
@@ -1,9 +1,66 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const title = 'My React App'
-const App = () => {
-    return <div>My React App</div>
+const SearchForm = () => {
+    return (
+        <form>
+            <input type="text" />
+            <button type="submit">search</button>
+        </form>
+    )
 }
+
+//const title = 'My React Application'
+const Header = (props) => (
+        <header>
+            <h1>{props.title}</h1>
+              <SearchForm />
+
+        </header>
+
+)
+const Items = (props) => {
+    console.log(props.items)
+    return (
+        <ul>
+        {props.items.map(item => (
+            <li>{item}</li>
+        ))}
+        </ul>
+    )
+}
+
+const Content = (props) => (
+    <section>
+        <p>{props.description}</p>
+        <Items items={props.items}/>
+    </section>
+)
+
+const AppWithoutDescription = () => (
+    <Header title = 'No description here'/>
+)
+
+const App = () => {
+    const title = 'Fronttechs: React'
+    const description = 'A simple react application'
+    const items = [
+        "Oliver",
+        "Tobey",
+        "Charkue",
+        "Lucky",
+        "Poo"
+    ]
+    return (
+        <section>
+          <Header title={title} />
+          <Content description={description}
+                items={items}/>
+        </section>
+        
+    )
+}
+
 const element = document.getElementById('app')
+//ReactDOM.render(<AppWithoutDescription />,element)
 ReactDOM.render(<App />,element)
